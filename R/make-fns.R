6e85ff04830f5401c89991ce5788d0773b8aca07
mpadge <mark.padgham@email.com>
2021-08-30 13:10:11 +0200

    add full param to rtd_clean

--- R/make-fns.R
+++ R/make-fns.R
@@ -18,8 +18,10 @@ rtd_build <- function (path = ".") {
 #' Clean the RTD `_build` directory
 #'
 #' @inheritParams r2readthedocs
+#' @param full If `TRUE`, also clean R package files, including vignettes and
+#' help files. Use \link{r2readthedocs} to re-generate these.
 #' @export
-rtd_clean <- function (path = ".") {
+rtd_clean <- function (path = ".", full = FALSE) {
 
     path <- convert_path (path)
     path_docs <- file.path (path, "docs")
@@ -30,6 +32,14 @@ rtd_clean <- function (path = ".") {
 
         ret <- withr::with_dir (path_docs,
                                 system2 ("make", args = "clean"))
+
+        if (full) {
+
+            dirs <- list.dirs (path_docs, recursive = FALSE)
+            dirs <- dirs [which (!grepl ("\\_build$", dirs))]
+            for (d in dirs)
+                chk <- unlink (d, recursive = TRUE)
+        }
     }
 
     invisible (ret)
