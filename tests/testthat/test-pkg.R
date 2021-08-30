5e71be0f60ea39aaff134ec4dc57a5967601faf1
mpadge <mark.padgham@email.com>
2021-08-30 10:11:50 +0200

    test single r2readthedocs() fn

--- tests/testthat/test-pkg.R
+++ tests/testthat/test-pkg.R
@@ -1,35 +1,38 @@
 
 source ("../pkg-skeleton.R")
 
-test_that("dummy package", {
+# explicit steps through each sub-fn of main r2readthedocs() fn:
+test_that("readthedocs sub-functions", {
 
               pkg_name <- paste0 (sample (c (letters, LETTERS), size = 8),
                                   collapse = "")
-              d <- make_dummy_pkg (pkg_name = pkg_name)
-              expect_true (dir.exists (d))
+              path <- make_dummy_pkg (pkg_name = pkg_name)
+              expect_true (dir.exists (path))
 
-              rb <- file.path (d, ".Rbuildignore")
+              expect_false (dir.exists (file.path (path, "docs")))
+
+              rb <- file.path (path, ".Rbuildignore")
               expect_false (file.exists (rb))
-              rignore_amend (d)
+              rignore_amend (path)
               expect_true (file.exists (rb))
 
-              expect_false (dir.exists (file.path (d, "docs")))
+              expect_false (dir.exists (file.path (path, "docs")))
 
-              expect_silent (convert_man (d))
-              expect_true (dir.exists (file.path (d, "docs")))
-              expect_true (dir.exists (file.path (d, "docs", "functions")))
+              expect_silent (convert_man (path))
+              expect_true (dir.exists (file.path (path, "docs")))
+              expect_true (dir.exists (file.path (path, "docs", "functions")))
 
-              flist <- list.files (file.path (d, "docs"))
+              flist <- list.files (file.path (path, "docs"))
               expect_false (any (grepl ("\\.md$", flist))) # no readme.md (-> random.md)
 
-              expect_silent (r <- convert_readme (d))
-              flist <- list.files (file.path (d, "docs"))
+              expect_silent (r <- convert_readme (path))
+              flist <- list.files (file.path (path, "docs"))
               expect_true (any (grepl ("\\.md$", flist)))
               expect_true (file.exists (r))
 
               # Then add hex to README:
-              chk <- file.copy ("../hex.png", file.path (d, "hex.png"))
-              r <- file.path (d, "README.md")
+              chk <- file.copy ("../hex.png", file.path (path, "hex.png"))
+              r <- file.path (path, "README.md")
               x <- brio::read_lines (r)
               x [1] <- paste0 (x [1], 
                                " <img src='hex.png' align='right' height=210 width=182>")
@@ -37,14 +40,31 @@ test_that("dummy package", {
 
               # TODO: That suppress warnings is because the hex logo is not yet
               # put in the proper place - do that!
-              r <- suppressWarnings (convert_readme (d))
+              r <- suppressWarnings (convert_readme (path))
               x <- brio::read_lines (r)
               expect_false (grepl ("<img src", x [1]))
               expect_true (grep ("<img src", x) [1] > 1L)
 
-              expect_false (dir.exists (file.path (d, "docs", "vignettes")))
-              expect_output (convert_vignettes (d))
-              expect_true (dir.exists (file.path (d, "docs", "vignettes")))
+              expect_false (dir.exists (file.path (path, "docs", "vignettes")))
+              expect_output (convert_vignettes (path))
+              expect_true (dir.exists (file.path (path, "docs", "vignettes")))
 
-              unlink (d, recursive = TRUE)
+              unlink (path, recursive = TRUE)
+})
+
+test_that ("readthedocs function", {
+
+              pkg_name <- paste0 (sample (c (letters, LETTERS), size = 8),
+                                  collapse = "")
+              path <- make_dummy_pkg (pkg_name = pkg_name)
+              expect_true (dir.exists (path))
+
+              expect_false (dir.exists (file.path (path, "docs")))
+
+              chk <- r2readthedocs (path, open = FALSE)
+
+              expect_true (dir.exists (file.path (path, "docs")))
+              expect_true (dir.exists (file.path (path, "docs", "_build")))
+              expect_true (dir.exists (file.path (path, "docs", "_build", "html")))
+              expect_true (file.exists (file.path (path, "docs", "_build", "html", "index.html")))
 })
